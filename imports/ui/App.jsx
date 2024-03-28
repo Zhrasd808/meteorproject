5a7f9d7c6f158f5e9d1e87f284448dbed4c02b0e
Zhrasd808 <zhrasadi@gmail.com>
2024-03-28 14:43:43 +0330

    hello git

---
+++ imports/ui/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+	<h2>to-do-app</h2>
+    <Hello/>
+    <Info/>
+  </div>
+);
