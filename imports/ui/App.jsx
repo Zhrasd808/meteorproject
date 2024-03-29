4da3a6895e3df6564682500130becbd8a6c29755
Zhrasd808 <zhrasadi@gmail.com>
2024-03-29 19:22:47 +0330

    v0.0.2

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { TasksCollection } from '/imports/api/TasksCollection';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Task } from './Task';
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-	<h2>to-do-app</h2>
-	<h3>v0.0.1</h3>
-    <Hello/>
-    <Info/>
-  </div>
-);
+const tasks = [
+	{_id: 0, text: 'First Task'},
+	{_id: 1, text:'Secend Task'},
+    {_id: 2, text:'Third Task' }
+];
+
+export const App = () => {
+    const tasks = useTracker(() => TasksCollection.find({}).fetch());
+
+	return(
+
+ 
+       <div>
+          <h1>Welcome to Meteor!</h1>
+	
+        	<ul>
+         	   { tasks.map(task => <Task key={ task._id } task={ task } />) }
+        	</ul>
+    
+      </div>
+    );
+};
